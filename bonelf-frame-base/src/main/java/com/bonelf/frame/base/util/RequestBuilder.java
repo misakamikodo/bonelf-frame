415d3b5ef8ddbbf35f96cb1eeed8f6560c2bfe65
bonelf <ccykirito@163.com>
2021-10-13 23:05:02 +0800

    添加一个请求工具类

---
+++ bonelf-frame-base/src/main/java/com/bonelf/frame/base/util/RequestBuilder.java
@@ -0,0 +1,188 @@
+package com.bonelf.frame.base.util;
+
+import com.bonelf.frame.core.domain.Result;
+import com.bonelf.frame.core.exception.BonelfException;
+import com.bonelf.frame.core.exception.enums.CommonBizExceptionEnum;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.lang.NonNull;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * 请求创建
+ * @author ccy
+ * @date 2021-10-11 6:03
+ */
+public class RequestBuilder {
+	private ObjectMapper objectMapper = new ObjectMapper();
+	private RestTemplate restTemplate = new RestTemplate();
+
+	private ProjectUrlEnum projectUrl = ProjectUrlEnum.WMS;
+
+	private HttpMethod method = HttpMethod.GET;
+	/**
+	 * 请求流数据
+	 */
+	private boolean stream;
+	/**
+	 * 表单提交/urlEncode 提交
+	 */
+	private boolean formData;
+	private HttpEntity<?> formEntity = new HttpEntity<>(
+			null, new HttpHeaders());
+
+	/**
+	 * 地址配置枚举
+	 */
+	public enum ProjectUrlEnum {
+		/**
+		 * 业务系统
+		 */
+		WMS("oh.wms.url"),
+		;
+
+		ProjectUrlEnum(String configKey) {
+			this.configKey = configKey;
+		}
+
+		private final String configKey;
+
+		public String getUrl() {
+			return configKey;
+		}
+	}
+
+	/*===========================构造===========================*/
+
+	public RequestBuilder restTemplate(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+		return this;
+	}
+
+	public RequestBuilder projectUrl(ProjectUrlEnum projectUrl) {
+		this.projectUrl = projectUrl;
+		return this;
+	}
+
+	public RequestBuilder formData() {
+		this.formData = true;
+		return this;
+	}
+
+	public RequestBuilder formData(ObjectMapper objectMapper) {
+		formData();
+		this.objectMapper = objectMapper;
+		return this;
+	}
+
+	public RequestBuilder method(HttpMethod method) {
+		this.method = method;
+		return this;
+	}
+
+	public RequestBuilder bearerToken(String bearerToken) {
+		this.formEntity.getHeaders().add("Authorization", "Bearer " + bearerToken);
+		return this;
+	}
+
+	public RequestBuilder stream() {
+		this.stream = true;
+		return this;
+	}
+
+	public RequestBuilder json() {
+		this.formData = false;
+		return this;
+	}
+
+	public RequestBuilder noData() {
+		return data(null);
+	}
+
+	public RequestBuilder data(Object data) {
+		HttpHeaders headers = formEntity.getHeaders();
+		if (!stream) {
+			headers.add("Accept", MediaType.APPLICATION_JSON.toString());
+		}
+		if (formData && !(data instanceof MultiValueMap)) {
+			TypeReference<HashMap<String, Object>> multiTypeReference =
+					new TypeReference<HashMap<String, Object>>() {
+					};
+			MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+			Map<String, Object> map = objectMapper.convertValue(data, multiTypeReference);
+			map.forEach(params::add);
+			formEntity = new HttpEntity<>(
+					params, headers);
+		} else {
+			formEntity = new HttpEntity<>(
+					data, headers);
+		}
+		return this;
+	}
+
+	/*===========================请求===========================*/
+
+	public <RES> Result<RES> send(@NonNull String url,
+								  ParameterizedTypeReference<Result<RES>> reference,
+								  Object... urlParams) {
+		if (formEntity == null) {
+			noData();
+		}
+		ResponseEntity<Result<RES>> data = restTemplate.exchange(
+				getSpecificUrl(url),
+				method,
+				formEntity,
+				reference,
+				urlParams
+		);
+		Result<RES> res = data.getBody();
+		if (res != null && !res.getSuccess()) {
+			throw new BonelfException(res.getCode(), res.getMessage(), res.getDevMessage());
+		} else {
+			return Optional.ofNullable(res).orElseThrow(() -> new BonelfException(CommonBizExceptionEnum.REQUEST_INVALIDATE));
+		}
+	}
+
+	public <RES> Result<RES> post(String url, ParameterizedTypeReference<Result<RES>> reference) {
+		this.method = HttpMethod.POST;
+		return send(url, reference);
+	}
+
+	public <RES> Result<RES> get(String url, ParameterizedTypeReference<Result<RES>> reference, Object... urlParams) {
+		this.method = HttpMethod.GET;
+		return send(url, reference, urlParams);
+	}
+
+	private byte[] streamReq(@NonNull String url, Object... urlParams) {
+		this.stream = true;
+		if (formEntity == null) {
+			noData();
+		}
+		ResponseEntity<byte[]> data = restTemplate.exchange(
+				getSpecificUrl(url),
+				method,
+				formEntity,
+				byte[].class,
+				urlParams
+		);
+		return data.getBody();
+	}
+
+	public byte[] getStream(String url, Object... urlParams) {
+		this.method = HttpMethod.GET;
+		return streamReq(url, urlParams);
+	}
+
+	private String getSpecificUrl(String url) {
+		return url;
+	}
+
+}
--- bonelf-frame-core/src/main/java/com/bonelf/frame/core/jackson/NullValueSerializer.java
+++ bonelf-frame-core/src/main/java/com/bonelf/frame/core/jackson/NullValueSerializer.java
@@ -5,7 +5,6 @@ import cn.hutool.core.util.StrUtil;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import org.springframework.boot.jackson.JsonComponent;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
@@ -22,22 +21,28 @@ public class NullValueSerializer extends JsonSerializer<Object> {
 		String fieldName = gen.getOutputContext().getCurrentName();
 		try {
 			//反射获取字段类型 如果转了下划线这里要转回驼峰取field
-			Field field = gen.getCurrentValue().getClass().getDeclaredField(fieldName);
-			if (Objects.equals(field.getType(), String.class)) {
+			Class<?> type;
+			if (gen.getCurrentValue().getClass().isAssignableFrom(Map.class)) {
+				type = ((Map<?, ?>)gen.getCurrentValue()).get(fieldName).getClass();
+			} else {
+				Field field = gen.getCurrentValue().getClass().getDeclaredField(fieldName);
+				type = field.getType();
+			}
+			if (Objects.equals(type, String.class)) {
 				//字符串null返回空字符串
 				gen.writeString(StrUtil.EMPTY);
 				return;
-			} else if (Objects.equals(field.getType(), Collection.class) || field.getType().isArray()) {
+			} else if (Objects.equals(type, Collection.class) || type.isArray()) {
 				//List字段如果为null,输出为[],而非null
 				gen.writeStartArray();
 				gen.writeEndArray();
 				return;
-			} else if (Objects.equals(field.getType(), Map.class)) {
+			} else if (Objects.equals(type, Map.class)) {
 				//map型空值返回{}
 				gen.writeStartObject();
 				gen.writeEndObject();
 				return;
-			} else if (Objects.equals(field.getType(), Boolean.class)) {
+			} else if (Objects.equals(type, Boolean.class)) {
 				//空布尔值返回false
 				gen.writeBoolean(false);
 				return;
